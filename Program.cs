096ce253693811be5620e94d4a311449f6114a46
Dirk Dobson <dirkvancouver@live.com>
2018-11-23 16:43:20 -0700

    reverse

--- Program.cs
+++ Program.cs
@@ -9,7 +9,17 @@ namespace csIteration
             for (var  i =1; i <= 10; i++)
             {
                 if (i % 2 == 0)
-                System.Console.WriteLine(i);
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if ( i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
